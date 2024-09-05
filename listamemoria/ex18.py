1aeba87aee1ddad7fbad3412b6ab46a55eca6411
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2024-09-05 01:13:09 -0300

    .

--- listamemoria/ex18.py
+++ listamemoria/ex18.py
@@ -1,7 +1,16 @@
 # Ordenação por seleção
 
 def ordena_selecao(lst: list[int]) -> None:
-    for i in range(0, len(lst)):
-        #for _ (0,)
-        i = i
-        
+    for i in range(len(lst)):
+        while i > 0 and lst[i - 1] > lst[i]:
+            # troca lst[i] <-> lst[i - 1]
+            t = lst[i]
+            lst[i] = lst[i - 1]
+            lst[i - 1] = t
+            i = i - 1
+
+lista = [8, 5, 4, 1, 2]
+ordena_selecao(lista)
+print(lista)
+
+# tenho que fazer alguma coisa para cortar a lista na repetição
