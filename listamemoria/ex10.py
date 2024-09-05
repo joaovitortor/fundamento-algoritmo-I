1bf4914b8d37abacdbd22b8ceaf4fe0dba83f843
João Vitor Bidoia Angelo <joaovitorbidoiaangelo@gmail.com>
2024-09-04 22:24:07 -0300

    .

--- listamemoria/ex10.py
+++ listamemoria/ex10.py
@@ -1,1 +1,19 @@
-print('oiii)
+# Projete uma função que modifique uma lista de números somando um valor n a cada um dos seus elementos
+
+def soma_n(lst: list[int], n: int) -> None:
+    '''
+    Modifica a lista *lst* somando *n* em cada um dos seus elemetos
+
+    >>> lst = []
+    >>> soma_n(lst, 5)
+    >>> print(lst)
+    []
+
+    >>> lst = [1, 2, 3]
+    >>> soma_n(lst, 5)
+    >>> print(lst)
+    [6, 7, 8]
+    '''
+
+    for i in range(0, len(lst)):
+        lst[i] = lst[i] + n
---
+++ listamemoria/ex11.py
@@ -0,0 +1,28 @@
+#  Projete uma função que modifique uma lista de strings deixando todas com o mesmo tamanho. Adiciona espaços em branco ao final das strings se necessário.
+
+def str_n(lst: list[str], n: int) -> None:
+    '''
+    Modifica os elementos de lst, deixando as str do mesmo tamanho *n*
+    >>> lst = []
+    >>> str_n(lst, 3)
+    >>> print(lst)
+    []
+
+    >>> lst = ['alou', 'joao', 'pedro']
+    >>> str_n(lst, 3)
+    >>> print(lst)
+    ['alo', 'joa', 'ped']
+
+    >>> lst = ['', 'a', 'b']
+    >>> str_n(lst, 3)
+    >>> print(lst)
+    ['   ', 'a  ', 'b  ']
+
+    '''
+
+    for i in range(0, len(lst)):
+        if len(lst[i]) < n:
+            espaco = ' ' * (n - len(lst[i]))
+            lst[i] = lst[i] + espaco
+        elif len(lst[i]) > n:
+            lst[i] = lst[i][:n]
